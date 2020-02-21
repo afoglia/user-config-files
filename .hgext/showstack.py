fc4ac8f4c058f16ae911142835bf3eddc40134d0
Anthony Foglia <afoglia@gmail.com>
2020-02-21 16:41:08 -0500

    Install hg showstack extension.
    Copied from revision 44323:76a96e3a2bbb
    https://www.mercurial-scm.org/repo/hg/file/default/contrib/showstack.py.

---
+++ .hgext/showstack.py
@@ -0,0 +1,31 @@
+# showstack.py - extension to dump a Python stack trace on signal
+#
+# binds to both SIGQUIT (Ctrl-\) and SIGINFO (Ctrl-T on BSDs)
+r"""dump stack trace when receiving SIGQUIT (Ctrl-\) or SIGINFO (Ctrl-T on BSDs)
+"""
+
+from __future__ import absolute_import, print_function
+import signal
+import sys
+import traceback
+
+
+def sigshow(*args):
+    sys.stderr.write("\n")
+    traceback.print_stack(args[1], limit=10, file=sys.stderr)
+    sys.stderr.write("----\n")
+
+
+def sigexit(*args):
+    sigshow(*args)
+    print('alarm!')
+    sys.exit(1)
+
+
+def extsetup(ui):
+    signal.signal(signal.SIGQUIT, sigshow)
+    signal.signal(signal.SIGALRM, sigexit)
+    try:
+        signal.signal(signal.SIGINFO, sigshow)
+    except AttributeError:
+        pass
